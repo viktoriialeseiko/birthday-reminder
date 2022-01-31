23ac63b53a8b7713210b8a081b5d5d0a006b5d99
viktoriialeseiko <vikyshaaa1@gmail.com>
2022-01-31 14:42:28 -0600

    button functionality added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
             <section className='container'>
                 <h3>{people.length} birthdays today</h3>
                 <List people={people} />
-                <button onClick={() => console.log('u clicked me')}>
-                    clear all
-                </button>
+                <button onClick={() => setPeople([])}>clear all</button>
             </section>
         </main>
     );
